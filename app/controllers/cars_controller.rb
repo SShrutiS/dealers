23ed96ee9f6c409f2b5387b46dce2fcc276c5b62
Shruti <shruti.sharma@mckesson.com>
2017-08-23 22:11:28 +0000

    craetevalidation

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,25 @@ class CarsController < ApplicationController
     def index
         @cars = Car.all
     end
+    def new
+        @car = Car.new
+    end
+    def create
+       @car = Car.new(params[:car].permit(:vin, :make, :model, :year)) 
+       if @car.save
+           redirect_to @car
+        else
+            render 'new'
+       end
+    end    
+    def show
+         @car = Car.find(params[:id])
+    end
+    def destroy
+        @car = Car.find(params[:id])
+        @car.destroy
+        redirect_to cars_path
+    end
+
+    
 end
--- spec/features/cars_spec.rb
+++ spec/features/cars_spec.rb
@@ -2,8 +2,46 @@ require 'rails_helper'
 
 RSpec.feature "Cars", type: :feature do
   scenario 'User views the /cars page' do  # scenario is what is happening from user's perspective
+      car = Car.first
       visit '/cars'
       expect(page).to have_text('Cars')
       expect(page).to have_text('Toyota')
+      expect(page.html).to include('<h1>Cars</h1>')
+      expect(page).to have_link('New', href: '/cars/new')
+      expect(page).to have_link(car.vin, href: "/cars/#{car.id}")
+  end
+  
+  scenario 'User views their car show page' do
+      car = Car.first
+      visit '/cars'
+      click_link car.vin
+      expect(page).to have_text(car.vin)
+      expect(page).to have_text(car.make)
+      expect(page).to have_text(car.model)
+      expect(page).to have_text(car.year)
+      expect(page).to have_link('Edit', href: "/cars/#{car.id}/edit")
+      expect(page).to have_link('Delete', href: "/cars/#{car.id}")
+  end
+  
+  scenario 'User delete the car' do
+       car = Car.first
+       visit '/cars'
+       click_link car.vin
+       click_link 'Delete'
+       expect(page).to have_text('Cars')
+       expect(page).to_not have_text(car.vin)
+   end
+    
+  scenario 'User creates a car' do
+    visit '/cars/new'
+    fill_in 'car[vin]', with: 'C0000000000000009'
+    fill_in 'car[make]', with: 'AMC'
+    fill_in 'car[model]', with: 'Gremlin'
+    fill_in 'car[year]', with: '1973'
+    click_button 'Create Car'
+    expect(page).to have_text('C0000000000000009')
+    expect(page).to have_text('AMC')
+    expect(page).to have_text('Gremlin')
+    expect(page).to have_text('1973')
   end
 end
